b52025571f4199b1fe67cabd304b36cb8a73c63c
傅清庭 <fuqt08@163.com>
2018-11-13 09:21:32 +0800

    up

---
+++ src/Word.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace fuqt08;
+
+class Word
+{
+    protected $db;
+
+    protected function __construct()
+    {
+
+    }
+
+    public function test()
+    {
+        return '123';
+    }
+}
