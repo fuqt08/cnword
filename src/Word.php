30b753a4b8f0e4864e18824ee5b4cc7b17ca53d5
傅清庭 <fuqt08@163.com>
2018-11-14 15:06:12 +0800

    up

--- src/Word.php
+++ src/Word.php
@@ -9,18 +9,22 @@ class Word
     /**
      * 查字相关信息
      * @param $zi
-     * @return bool|array
+     * @return bool|array|string
      */
-    public static function find($zi)
+    public static function find($zi, $field = '')
     {
         $config = [
             'database_type' => 'sqlite',
             'database_file' => __DIR__ . '/data/dict.db'
         ];
+        $fieldList = ['zi', 'big5', 'bihua', 'bihua2', 'py', 'py2', 'shengmu', 'yunmu', 'bushou', 'wx', 'wubi'];
         $dataBase = new Medoo($config);
-        $data = $dataBase->get('bw_cnword', '*', ['zi' => $zi]);
+        $data = $dataBase->get('bw_cnword', $fieldList, ['zi' => $zi]);
         if (empty($data)) {
             return false;
+        }
+        if (in_array($field, $fieldList)) {
+            return $data[$field];
         }
         return $data;
     }
