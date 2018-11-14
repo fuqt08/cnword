bad8ecdb8b3a0327f8674ce5d67105c95fcea9e3
傅清庭 <fuqt08@163.com>
2018-11-14 10:42:35 +0800

    up

--- src/Word.php
+++ src/Word.php
@@ -2,17 +2,22 @@
 
 namespace fuqt08;
 
+use Medoo\Medoo;
+
 class Word
 {
-    protected $db;
 
-    protected function __construct()
+    public static function find($zi)
     {
-
-    }
-
-    public function test()
-    {
-        return '123';
+        $config = [
+            'database_type' => 'sqlite',
+            'database_file' => __DIR__ . '/data/dict.db'
+        ];
+        $dataBase = new Medoo($config);
+        $data = $dataBase->get('bw_cnword', '*', ['zi' => $zi]);
+        if (empty($data)) {
+            return false;
+        }
+        return $data;
     }
 }
